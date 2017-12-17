fb22e3fabf88b477645505b148fd9bc3f1ba3a2e
Vijay Chavda <VijayChavda@gmail.com>
2017-12-17 15:18:54 +0530

    Create project

---
+++ declone/src/me/vijaychavda/Main.java
@@ -0,0 +1,16 @@
+package me.vijaychavda;
+
+/**
+ *
+ * @author Vijay
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
