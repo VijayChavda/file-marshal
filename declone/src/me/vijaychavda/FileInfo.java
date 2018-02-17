bfd26fe30f159f7a03ad96690257ab515ed1ef47
Vijay Chavda <VijayChavda@gmail.com>
2018-02-17 13:43:33 +0530

    Add file type field to FileInfo.

--- declone/src/me/vijaychavda/FileInfo.java
+++ declone/src/me/vijaychavda/FileInfo.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.zip.Adler32;
 import org.apache.commons.lang3.SerializationUtils;
 
@@ -14,17 +12,19 @@ public class FileInfo {
 
     private final String path;
     private String name;
+    private String type;
     private final long size;
     private final long hash;
 
     private FileInfo() {
         path = "<Not initialized>";
         name = "<Not initialized>";
+        type = "<Not initialized>";
         size = -1;
         hash = -1;
     }
 
-    private FileInfo(String path, String name, long size, long hash) {
+    private FileInfo(String path, String name, String type, long size, long hash) {
         this.path = path;
         this.name = name;
         this.size = size;
@@ -37,6 +37,10 @@ public class FileInfo {
 
     public String getName() {
         return name;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public long getSize() {
@@ -52,24 +56,33 @@ public class FileInfo {
     }
 
     public static FileInfo get(String path) throws IOException {
+        //<editor-fold defaultstate="collapsed" desc="variables">
         File file = new File(path);
+        long fileSize = file.length();
+        String fileName = file.getName();
+
+        int i = fileName.lastIndexOf('.');
+        String fileType = i > 0 ? fileName.substring(i + 1) : "";
+        //</editor-fold>
 
         //<editor-fold defaultstate="collapsed" desc="Case 1: No need to check content">
         if (AppContext.Current.getCompareSettings().isUsingContent() == false)
-            return new FileInfo(path, file.getName(), file.length(), -1);
+            return new FileInfo(path, fileName, fileType, fileSize, -1);
         //</editor-fold>
 
+        //<editor-fold defaultstate="collapsed" desc="more variables">
         Adler32 adler = new Adler32();
         CompareSettings settings = AppContext.Current.getCompareSettings();
 
         int partScanLength = 1048576;
-        long totalLength = file.length();
+        long totalLength = fileSize;
         double scanPercent
             = settings.isContent2p() ? 2
             : settings.isContent10p() ? 10
             : settings.isContent20p() ? 20
             : settings.isContent50p() ? 50 : 100;
         long scanLength = Math.round(totalLength * scanPercent / 100);
+        //</editor-fold>
 
         //<editor-fold defaultstate="collapsed" desc="Case 2: File is very small">
         if (scanLength < partScanLength) {
@@ -81,13 +94,15 @@ public class FileInfo {
 
             adler.update(data);
 
-            return new FileInfo(path, file.getName(), file.length(), adler.getValue());
+            return new FileInfo(path, fileName, fileType, fileSize, adler.getValue());
         }
         //</editor-fold>
 
+        //<editor-fold defaultstate="collapsed" desc="even more variables">
         long partitions = scanLength / partScanLength;
         long partLength = totalLength / partitions;
         long skipLength = partLength - partScanLength;
+        //</editor-fold>
 
         //<editor-fold defaultstate="collapsed" desc="Case 3: File is large">
         HashSet<Long> partialHashes = new HashSet<>();
@@ -111,9 +126,9 @@ public class FileInfo {
 
         byte[] hashBytes = SerializationUtils.serialize(partialHashes);
         adler.update(hashBytes);
-        //</editor-fold>
 
-        return new FileInfo(path, file.getName(), file.length(), adler.getValue());
+        return new FileInfo(path, fileName, fileType, fileSize, adler.getValue());
+        //</editor-fold>
     }
 
     @Override
