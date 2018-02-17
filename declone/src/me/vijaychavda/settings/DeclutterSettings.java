2fe4e660804a2c956074d4fbd56049f27e0d4581
Vijay Chavda <VijayChavda@gmail.com>
2018-02-17 17:06:42 +0530

    Add Declutter settings.

---
+++ declone/src/me/vijaychavda/settings/DeclutterSettings.java
@@ -0,0 +1,32 @@
+package me.vijaychavda.settings;
+
+public class DeclutterSettings {
+
+    private int minimumGroupCardinality;
+    private String outputPath;
+    private String grouping;
+
+    public int getMinimumGroupCardinality() {
+        return minimumGroupCardinality;
+    }
+
+    public void setMinimumGroupCardinality(int minimumGroupCardinality) {
+        this.minimumGroupCardinality = minimumGroupCardinality;
+    }
+
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public void setOutputPath(String outputPath) {
+        this.outputPath = outputPath;
+    }
+
+    public String getGrouping() {
+        return grouping;
+    }
+
+    public void setGrouping(String grouping) {
+        this.grouping = grouping;
+    }
+}
