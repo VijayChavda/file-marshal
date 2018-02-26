ee0795c8f5c38197ce87c0415ca0c5f65bb97b7e
Vijay Chavda <vijaychavda@gmail.com>
2018-02-26 12:15:00 +0530

    Resolve clash with previous clutter.

--- File Marshal/src/me/vijaychavda/filemarshal/Main.java
+++ File Marshal/src/me/vijaychavda/filemarshal/Main.java
@@ -1,11 +1,15 @@
 package me.vijaychavda.filemarshal;
 
 import java.awt.EventQueue;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import me.vijaychavda.filemarshal.ui.MainFrame;
+import me.vijaychavda.filemarshal.workers.DeclutterWorker;
 
 public class Main {
 
@@ -17,8 +21,23 @@ public class Main {
         }
 
         EventQueue.invokeLater(() -> {
+//            deleteDir(new File("/home/v/Desktop/Decluttered/"));
             new MainFrame().setVisible(true);
         });
+    }
+    
+    private static void deleteDir(File file) {
+        try {
+            File[] contents = file.listFiles();
+            if (contents != null) {
+                for (File f : contents) {
+                    deleteDir(f);
+                }
+            }
+            Files.delete(file.toPath());
+        } catch (IOException e) {
+            Logger.getLogger(DeclutterWorker.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
 }
